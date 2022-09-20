cf69dac9508799d62a8f37f399b880f41444da55
TerribleVillageois <maximeboivin31416@gmail.com>
2022-09-20 15:05:10 -0400

    Update

--- MvcMovie/HelloWorldController.cs
+++ MvcMovie/HelloWorldController.cs
@@ -7,18 +7,17 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    public string Index(string name, int ID= 1)
     {
-        return "This is my default action....";
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID : {ID}");
     }
     
     // Get: /HelloWorld/Welcome
     // Requires using System.Text.Encodings.Web;
     public string Welcome(string name, int numTimes = 1)
     {
-        string phrase = String.Concat(Enumerable.Repeat(name + " ",numTimes));
-        return phrase;
+        //string phrase = String.Concat(Enumerable.Repeat(name + " ",numTimes));
+        //return phrase;
         return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is : {numTimes}");
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
     }
 }
